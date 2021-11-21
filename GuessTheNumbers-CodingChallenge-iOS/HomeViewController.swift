7e1ba04b6cd980d71071b48c19d1df2af8441f0d
karyRmartinez <karymartinez@pursuit.org>
2021-11-21 01:48:34 -0500

    implementing TextFields to stackView

--- GuessTheNumbers-CodingChallenge-iOS/HomeViewController.swift
+++ GuessTheNumbers-CodingChallenge-iOS/HomeViewController.swift
@@ -27,18 +27,61 @@ class MainViewController: UIViewController {
         label.text = "Enter 4 digit combination"
         return label
     }()
+    lazy var boxOneTextField: UITextField = {
+           let textField = UITextField()
+           textField .translatesAutoresizingMaskIntoConstraints = false
+           textField .borderStyle = .none
+           textField .backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+           textField.layer.borderColor = UIColor.black.cgColor
+           return textField
+       }()
+    lazy var boxTwoTextField: UITextField = {
+           let textField = UITextField()
+           textField .translatesAutoresizingMaskIntoConstraints = false
+           textField .borderStyle = .none
+           textField .backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+           textField.layer.borderColor = UIColor.black.cgColor
+           return textField
+       }()
+    lazy var boxThreeTextField: UITextField = {
+           let textField = UITextField()
+           textField .translatesAutoresizingMaskIntoConstraints = false
+           textField .borderStyle = .none
+           textField .backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+           textField.layer.borderColor = UIColor.black.cgColor
+           return textField
+       }()
+    lazy var boxFourTextField: UITextField = {
+           let textField = UITextField()
+           textField .translatesAutoresizingMaskIntoConstraints = false
+           textField .borderStyle = .none
+           textField .backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+           textField.layer.borderColor = UIColor.black.cgColor
+           return textField
+       }()
     //MARK: SubViews
     func addSubView() {
         self.view.addSubview(titlelabel)
         self.view.addSubview(instructionsLabel)
-        self.view.addSubview(stackView)
+        self.view.addSubview(boxOneTextField)
+        self.view.addSubview(boxTwoTextField)
+        self.view.addSubview(boxThreeTextField)
+        self.view.addSubview(boxFourTextField)
     }
-    
+    //MARK: Private Functions
+    // ToDo: Center number input. Fix StackView Constraints to be centered.
     private func configureStackView() {
+        let stackView = UIStackView(arrangedSubviews: [boxOneTextField,boxTwoTextField,boxThreeTextField,boxFourTextField])
         stackView.axis  = .horizontal
         stackView.distribution = .fillEqually
         stackView.spacing = 20
-        setStackViewConstraints()
+        self.view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints    = false
+        stackView.topAnchor.constraint(equalTo: instructionsLabel.safeAreaLayoutGuide.topAnchor, constant: 200).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant:-20).isActive = true
+       stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -350).isActive = true
     }
     
     //MARK: Cycle
@@ -49,27 +92,21 @@ class MainViewController: UIViewController {
         configureStackView()
         setTitleLabelConstraints()
         setInstructionLabelConstraints()
-        
+    
         
     }
     //MARK: CONSTRAINTS
     func setTitleLabelConstraints() {
          titlelabel.translatesAutoresizingMaskIntoConstraints    = false
-         titlelabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+         titlelabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
          titlelabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
          titlelabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant:-20).isActive = true
     }
     func setInstructionLabelConstraints() {
          instructionsLabel.translatesAutoresizingMaskIntoConstraints    = false
-         instructionsLabel.topAnchor.constraint(equalTo: titlelabel.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+         instructionsLabel.topAnchor.constraint(equalTo: titlelabel.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
          instructionsLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
          instructionsLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant:-20).isActive = true
     }
-    func setStackViewConstraints() {
-         stackView.translatesAutoresizingMaskIntoConstraints    = false
-         stackView.topAnchor.constraint(equalTo: instructionsLabel.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
-         stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
-         stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant:-20).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30).isActive = true
-    }
+    
 }

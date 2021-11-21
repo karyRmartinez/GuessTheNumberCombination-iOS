f0d31a8d6db6b468aa478a6c38b4ed8e0b730b42
karyRmartinez <karymartinez@pursuit.org>
2021-11-21 00:21:51 -0500

    instructionlabel UI and constraints

--- GuessTheNumbers-CodingChallenge-iOS/HomeViewController.swift
+++ GuessTheNumbers-CodingChallenge-iOS/HomeViewController.swift
@@ -11,16 +11,24 @@ import UIKit
 class MainViewController: UIViewController {
     
     //MARK: UIObjects
-    lazy var Titlelabel: UILabel = {
+    lazy var titlelabel: UILabel = {
         let label = UILabel()
             label.textAlignment = .center
             label.font = UIFont.boldSystemFont(ofSize: 20)
             label.text = "Guess The Combination"
+            return label
+    }()
+    lazy var instructionsLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.text = "Enter 4 digit combination"
         return label
     }()
     
     func addSubView() {
-        self.view.addSubview(Titlelabel)
+        self.view.addSubview(titlelabel)
+        self.view.addSubview(instructionsLabel)
     }
     
     override func viewDidLoad() {
@@ -28,13 +36,20 @@ class MainViewController: UIViewController {
         view.backgroundColor = .white
         addSubView()
         setTitleLabelConstraints()
+        setInstructionLabelConstraints()
         
     }
     //MARK: CONSTRAINTS
     func setTitleLabelConstraints() {
-         Titlelabel.translatesAutoresizingMaskIntoConstraints    = false
-         Titlelabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-         Titlelabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
-         Titlelabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant:-20).isActive = true
+         titlelabel.translatesAutoresizingMaskIntoConstraints    = false
+         titlelabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+         titlelabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+         titlelabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant:-20).isActive = true
+    }
+    func setInstructionLabelConstraints() {
+         instructionsLabel.translatesAutoresizingMaskIntoConstraints    = false
+         instructionsLabel.topAnchor.constraint(equalTo: titlelabel.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+         instructionsLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+         instructionsLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant:-20).isActive = true
     }
 }

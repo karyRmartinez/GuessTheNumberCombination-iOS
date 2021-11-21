8a2511bd68cc5daef894de459ddf8ab4ac10fad1
karyRmartinez <karymartinez@pursuit.org>
2021-11-21 12:48:04 -0500

    configuring UIBUtton

--- GuessTheNumbers-CodingChallenge-iOS/CustomViews/GTNButton.swift
+++ GuessTheNumbers-CodingChallenge-iOS/CustomViews/GTNButton.swift
@@ -11,17 +11,24 @@ class GTNButton: UIButton {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    init(backgrounfColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
     }
-    */
 
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }

b46bf9175d7f3a9c86660e96e3113dc77086613c
karyRmartinez <karymartinez@pursuit.org>
2021-11-20 23:24:54 -0500

    navigation Controller adjustment

---
+++ GuessTheNumbers-CodingChallenge-iOS/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  GuessTheNumbers-CodingChallenge-iOS
+//
+//  Created by Kary Martinez on 11/20/21.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           view.backgroundColor = .white
+         
+       }
+}

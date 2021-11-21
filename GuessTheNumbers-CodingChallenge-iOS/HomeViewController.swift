49d31609c3e7e8250462c4acf2ae71c4a9029722
karyRmartinez <karymartinez@pursuit.org>
2021-11-21 00:11:13 -0500

    Title label UI and constraints

--- GuessTheNumbers-CodingChallenge-iOS/HomeViewController.swift
+++ GuessTheNumbers-CodingChallenge-iOS/HomeViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    //MARK: UIObjects
+    lazy var Titlelabel: UILabel = {
+        let label = UILabel()
+            label.textAlignment = .center
+            label.font = UIFont.boldSystemFont(ofSize: 20)
+            label.text = "Guess The Combination"
+        return label
+    }()
+    
+    func addSubView() {
+        self.view.addSubview(Titlelabel)
+    }
     
     override func viewDidLoad() {
-           super.viewDidLoad()
-           view.backgroundColor = .white
-         
-       }
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        addSubView()
+        setTitleLabelConstraints()
+        
+    }
+    //MARK: CONSTRAINTS
+    func setTitleLabelConstraints() {
+         Titlelabel.translatesAutoresizingMaskIntoConstraints    = false
+         Titlelabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+         Titlelabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+         Titlelabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant:-20).isActive = true
+    }
 }

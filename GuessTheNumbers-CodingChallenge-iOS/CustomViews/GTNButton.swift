f7ea0c0f403af61496cf2d9756b316d997eefe33
karyRmartinez <karymartinez@pursuit.org>
2021-11-21 12:37:18 -0500

    setting up CustomVIew- UIButton

---
+++ GuessTheNumbers-CodingChallenge-iOS/CustomViews/GTNButton.swift
@@ -0,0 +1,27 @@
+//
+//  GTNButton.swift
+//  GuessTheNumbers-CodingChallenge-iOS
+//
+//  Created by Kary Martinez on 11/21/21.
+//
+
+import UIKit
+
+class GTNButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

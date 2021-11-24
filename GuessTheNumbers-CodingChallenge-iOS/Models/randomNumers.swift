9ba74bfd7235bf90e72b212dfdfed9961ca1d467
karyRmartinez <karymartinez@pursuit.org>
2021-11-23 20:53:12 -0500

    creating networking and model files

---
+++ GuessTheNumbers-CodingChallenge-iOS/Models/randomNumers.swift
@@ -0,0 +1,15 @@
+//
+//  randomNumers.swift
+//  GuessTheNumbers-CodingChallenge-iOS
+//
+//  Created by Kary Martinez on 11/23/21.
+//
+
+import Foundation
+
+struct numbers: Codable {
+    var numberOne: Int
+    var numberTwo: Int
+    var numberThree: Int
+    var numberFour: Int
+}
---
+++ GuessTheNumbers-CodingChallenge-iOS/Networking/APIClient.swift
@@ -0,0 +1,8 @@
+//
+//  APIClient.swift
+//  GuessTheNumbers-CodingChallenge-iOS
+//
+//  Created by Kary Martinez on 11/23/21.
+//
+
+import Foundation
